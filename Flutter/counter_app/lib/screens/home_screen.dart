7605be1d169ec0aed0f310024c6db5d3b9c19421
David Medina Orozco <davidmedinaoro@gmail.com>
2022-03-13 23:20:06 -0500

    App Bar

--- Flutter/counter_app/lib/screens/home_screen.dart
+++ Flutter/counter_app/lib/screens/home_screen.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw const Center(child: Text("Hola Screen"));
+    return Scaffold(
+        appBar: AppBar(
+          title: const Center(child: Text("Hola Screen")),
+          elevation: 15.0,
+        ),
+        body: const Center(child: Text("Hola Screen")));
   }
 }
